a9febd19aecb5ed92041a41c960cc5b3d84ab0cc
liangxwei <liangxwei1030@163.com>
2019-05-23 00:42:51 +0800

    get activity detail

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,4 +1,6 @@
 // pages/detail/detail.js
+const request = require('../../request/index.js');
+const formatTime = require('../../utils/time.js');
 var base64 = require("../images/base64");
 Page({
 
@@ -6,7 +8,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    activity: {}
   },
 
   /**
@@ -17,7 +19,8 @@ Page({
       timeIcon: base64.timeIcon,
       locationIcon: base64.locationIcon,
       userIcon: base64.userIcon
-    })
+    });
+    this.getActivityDetail(options.id);
   },
 
   /**
@@ -67,5 +70,23 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  // 获取活动详情
+  getActivityDetail: function (id) {
+    var that = this;
+    var token = wx.getStorageSync('token');
+    request.getActivityDetail(token, id, function(err, res) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      // set data
+      res.data.startTime = formatTime.formatTime(res.data.startTime);
+      res.data.endTime = formatTime.formatTime(res.data.endTime);
+      that.setData({
+        activity: res.data
+      });
+    })
   }
 })

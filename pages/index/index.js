44ee7a5eb109a2c354fbf38ac6b9a6e768535bf7
yimin.space <yimin.space@bytedance.com>
2019-05-22 23:52:00 +0800

    index

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,13 +2,14 @@ const loginUtil = require('../../utils/login.js');
 const request = require('../../request/index.js');
 const formatTime = require('../../utils/time.js');
 
+var SHOE_TIMES = 0;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    hasAuth: true,
+    hasAuth: false,
     activityList: [],
   },
 
@@ -16,24 +17,20 @@ Page({
    * 生命周期函数--监听页面ß加载
    */
   onLoad: function(options) {
+    console.log('load');
+    this.afertLoad();
+  },
+
+  // afterLoad
+  afertLoad: function () {
     var that = this;
-    loginUtil.login(function(err, token) {
+    loginUtil.login(function (err, token) {
       if (err) {
         console.log(err);
         return;
       }
-      request.getActivityList(token, function(err, res) {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        const list = res.data.forEach(function (item) {
-          item.startTime = formatTime.formatTime(item.startTime)
-        })
-        that.setData({
-          activityList: res.data
-        });
-      })
+      that.getActivityList(token);
+      that.getUserInfo(token);
     })
   },
 
@@ -48,7 +45,12 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    console.log('show');
+    if (SHOE_TIMES > 1) {
+      var token = wx.getStorageSync('token');
+      this.getActivityList(token);
+    }
+    SHOE_TIMES++;
   },
 
   /**
@@ -78,13 +80,6 @@ Page({
   onReachBottom: function() {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  },
   gotoCreate: function() {
     wx.navigateTo({
       url: '/pages/create/create',
@@ -104,12 +99,99 @@ Page({
   },
   // 分享
   onShareAppMessage: function(e) {
+
     console.log(e.target.dataset.id);
+    if (!e.target) {
+      return {
+        title: '简单，方便的发起报名，就在橙子报名',
+        path: 'pages/index/index',
+        imageUrl: 'https://cdn.webfem.com/webfem/5abf6aa0-7c9b-11e9-b71c-8991f1dd05b3',
+      }
+    }
     const activityId = e.target.dataset.id;
     const activityTitle = e.target.dataset.title;
     return {
-      title: activityTitle,
-      path: 'pages/shared/shared?id=' + activityId
+      title: '我在橙子报名发起了一个活动，快来参加吧',
+      path: 'pages/shared/shared?id=' + activityId,
+      imageUrl: 'https://cdn.webfem.com/webfem/5abf6aa0-7c9b-11e9-b71c-8991f1dd05b3',
     }
+  },
+  // 获取所有列表
+  getActivityList: function(token) {
+    var that = this;
+    request.getActivityList(token, function(err, res) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      // 这里是说token失效
+      if (res.code !== 0) {
+        loginUtil.codeToken(function(err, token) {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          that.getActivityList(token);
+          that.getUserInfo(token);
+        })
+        return;
+      }
+      const list = res.data.forEach(function(item) {
+        item.startTime = formatTime.formatTime(item.startTime)
+      })
+      that.setData({
+        activityList: res.data
+      });
+    })
+  },
+  // 获取用户信息
+  getUserInfo: function(token) {
+    var that = this;
+    request.getUserInfo(token, function(err, data) {
+      if (err) {
+        that.setData({
+          hasAuth: false,
+        })
+        return;
+      }
+      
+      if (data.data && data.data.user.username) {
+        that.setData({
+          hasAuth: true,
+        })
+        return;
+      }
+      that.setData({
+        hasAuth: false,
+      })
+    })
+  },
+  // 微信授权
+  handleGetUserInfo: function(userInfo) {
+    console.log(userInfo);
+    var that = this;
+    if (userInfo.detail) {
+      that.setData({
+        hasAuth: true
+      })
+      const rawData = JSON.parse(userInfo.detail.rawData);
+      var data = {
+        username: rawData.nickName,
+        profile: rawData.avatarUrl,
+      }
+      that.updateUserInfo(data)
+    }
+  },
+  // 更新用户信息
+  updateUserInfo: function(data) {
+    var token = wx.getStorageSync('token');
+    if (!token) return;
+
+    request.updateUserInfo(token, data, function(err, data) {
+      console.log(data);
+      wx.navigateTo({
+        url: '/pages/create/create',
+      })
+    })
   }
 })
--- utils/login.js
+++ utils/login.js
@@ -3,10 +3,10 @@ const request = require('../request/index.js');
 module.exports = {
   // 调用此函数，返回用户信息，没有就会创建
   login: function (callback) {
+    var that = this;
     wx.getStorage({
       key: 'token',
       success: function(res) {
-        console.log(res)
         var token = res.data || '';
         if (token) {
           // 根据token拉一遍用户信息，防止token过期
@@ -14,14 +14,34 @@ module.exports = {
           callback(null, token)
         } else {
           // 调登录
-          codeToken(callback)
+          that.codeToken(callback)
         }
       },
       fail: function (e) {
-        codeToken(callback)
+        that.codeToken(callback)
       }
     })
   },
+  codeToken: function (callback) {
+    wxLogin(function (err, code) {
+      if (err) {
+        callback(err)
+        return;
+      }
+      // 拿code请求token
+      request.getTokenWithCode(code, function (err, data) {
+        if (err) {
+          callback(err)
+          return;
+        }
+        wx.setStorage({
+          key: 'token',
+          data: data.token,
+        })
+        callback(null, data.token)
+      })
+    })
+  }
 }
 
 function wxLogin(cb) {
@@ -36,27 +56,6 @@ function wxLogin(cb) {
     fail: function (err) {
       cb(err)
     }
-  })
-}
-
-function codeToken(callback) {
-  wxLogin(function (err, code) {
-    if (err) {
-      callback(err)
-      return;
-    }
-    // 拿code请求token
-    request.getTokenWithCode(code, function (err, data) {
-      if (err) {
-        callback(err)
-        return;
-      }
-      wx.setStorage({
-        key: 'token',
-        data: data.token,
-      })
-      callback(null, data.token)
-    })
   })
 }
 
